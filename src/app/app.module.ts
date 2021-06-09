328a1a6bd33e300cfddc1e6e7bf1000c21d3d4d5
matheus <matheus_jordan@yahoo.com.br>
2021-06-08 21:09:30 -0300

    Montando estrutura do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EMenuMaterialModule } from './material.module';
+import { EMenuMaterialModule } from './material/material.module';
+import { ClientInfoComponent } from './client-info/client-info.component';
+import { OrderInfoComponent } from './order-info/order-info.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientInfoComponent,
+    OrderInfoComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    EMenuMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
